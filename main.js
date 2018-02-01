62e5b1dd2870f40601dbd25986697efda15c372b
Rajiv Shah <rajivshah1@icloud.com>
2018-01-31 19:12:21 -0500

    Update for January 28 snapshot

--- main.js
+++ main.js
@@ -19,14 +19,13 @@ var getPreTransitionIota = function() {
 
 var IOTA = require('iota.lib.js');
 var iota = new IOTA({
-  'provider': 'http://iotanode.us:443'
+  'provider': 'https://iotanode.us:443'
 });
 var iotaOld = getPreTransitionIota();
 var seed = process.argv[2] + "" // in case the seed is all 9's (GOSH I HOPE NOT)
 var searchPreTransitioned = process.argv[3] === 'p';
 var status = 'checking'
-var snapshotSep = fs.readFileSync('snapshot_september.txt').toString().split("\n");
-var snapshotOct = fs.readFileSync('snapshot_october.txt').toString().split("\n");
+var snapshotJan = fs.readFileSync('Snapshot.txt').toString().split("\n");
 
 if (seed.length !== 81) {
   if(seed.length < 81){
@@ -60,35 +59,19 @@ var getNewAddressCallback = function(e, d, index, amountToScan) {
   var hits = 0
   for (var i = 0; i < d.length; i++) {
     var addr = d[i]
-    var hitSep = snapshotSep.filter((s) => {
-      return s.indexOf(addr) > -1
-    })
-    var hitOct = snapshotOct.filter((s) => {
+    var hit = snapshotJan.filter((s) => {
       return s.indexOf(addr) > -1
     })
 
-    if (hitSep.length > 0 || hitOct.length > 0) {
+    if (hit.length > 0 || hit.length > 0) {
       var snapshotDate = ""
       var balance = 0
-      var reason = ""
-      if(hitSep.length > 0){
-        snapshotDate = "September 22"
-        balance = parseInt(snapshotSep[snapshotSep.indexOf(hitSep[0])].split(',"balance":')[1])
-        reason = snapshotSep[snapshotSep.indexOf(hitSep[0])].split(',"category":')[1]
-      }
-      if(hitOct.length > 0){
-        snapshotDate = "October 23"
-        balance = parseInt(snapshotOct[snapshotOct.indexOf(hitOct[0])].split("; ")[1])
-        reason = snapshotOct[snapshotOct.indexOf(hitOct[0])].split(`${balance}; `)[1]
+      if(hit.length > 0){
+        balance = parseInt(snapshotJan[snapshotJan.indexOf(hit[0])].split(";")[1])
+        var convertedBalance = balance / 1000000
+        console.log(`Got a hit! ${addr} has a balance of ${convertedBalance} Mi.`)
       }
 
-      var convertedBalance = balance / 1000000
-      if (reason.match(/AVAILABLE/g) || reason.match(/NONE/g)) {
-        console.log(`Got a hit! ${addr} has a balance of ${convertedBalance} Mi. The balance is currently in the address.`)
-      }
-      else {
-        console.log(`Got a hit! ${addr} has a balance of ${convertedBalance} Mi which was found in the snapshot taken on ${snapshotDate}. The reason was ${reason}`)
-      }
         totalBalance += balance
         addressesWithBalances.push({
           address: addr,
